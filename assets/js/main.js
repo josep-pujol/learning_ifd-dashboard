ed282b51430020abf9ea27538458e31a55830416
Josep <pujol.rabat@gmail.com>
2019-05-06 21:15:55 +0000

    simple call to API

---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+
+const baseURL = "https://api.exchangerate-api.com/v4/latest/EUR"
+
+function getData(url_param, cb) {
+    var xhr = new XMLHttpRequest();
+    var url = baseURL; // + url_param + "?" + currencies;
+    console.log(url);
+    xhr.open("GET", url);
+    xhr.send();
+
+    xhr.onreadystatechange = function() {
+        console.log(this.readyState);
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+}
+
+
+function writeToDocument(url_param) {
+
+    getData(url_param, function(data) {
+        var doc_data = "";
+        console.dir(data);
+        console.log('data', data);
+        console.log('data.rates', data.rates);
+        data = data.rates;
+
+        for (var key in data) {
+            if (data.hasOwnProperty(key)) {
+                console.log(key + ": " + data[key]);
+                doc_data += "<p>" + key + ": " + data[key] + "</p>";
+            }
+        }
+
+
+        console.log('doc_data', doc_data);
+
+        var el = document.getElementById("testAPI");
+        el.innerHTML = ""
+        el.innerHTML = doc_data;
+
+    });
+}
+
+var url_param = "";
+writeToDocument(url_param)
