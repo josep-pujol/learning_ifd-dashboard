67d238837262d4d4a4c33c4e67f56b854b0ec879
Josep <pujol.rabat@gmail.com>
2019-05-09 00:30:03 +0000

    refractoring

--- assets/js/currency_converter.js
+++ assets/js/currency_converter.js
@@ -9,7 +9,6 @@ function getData(url_param, cb) {
     xhr.send();
 
     xhr.onreadystatechange = function() {
-        console.log(this.readyState);
         if (this.readyState == 4 && this.status == 200) {
             cb(JSON.parse(this.responseText));
         }
@@ -19,41 +18,33 @@ function getData(url_param, cb) {
 
 function writeToDocument(url_param) {
     
+    var el = document.getElementById("converted-currency");
+    el.innerHTML = "";
+    
     var amountToConvert = document.getElementById("amount").value.replace(/,/g, '.');
     var fromCurrency = document.getElementById("from-currency").value;
     var toCurrency = document.getElementById("to-currency").value;
     console.log('Data from Form: ', amountToConvert, fromCurrency, toCurrency);
     
     if (isNaN(amountToConvert)) {
-        var el = document.getElementById("converted-currency");
-        el.innerHTML = "";
         el.innerHTML = "<p>The value added in the box is not a valid number</p>";
         return el.innerHTML;
     }
-    
-    
-    var param_latest = `latest?base=${fromCurrency}&symbols=${toCurrency}`;
-    
-    
-    if ( fromCurrency == toCurrency ) {
-        
-        var el = document.getElementById("converted-currency");
-        el.innerHTML = "";
-        el.innerHTML = `<p>${amountToConvert}</p>`;
+
+
+    if ( amountToConvert == 0 | fromCurrency == toCurrency ) {
+        el.innerHTML = `<p>${amountToConvert*1}</p>`;
         
     } else {
-    
+        var param_latest = `latest?base=${fromCurrency}&symbols=${toCurrency}`;
+        
         getData(param_latest, function(data) {
             var doc_data = "";
             console.dir(data);
-            console.log('data', data);
-            console.log('data.rates', data.rates);
             data = data.rates;
     
-            var convertedCurrency = amountToConvert * data[toCurrency];
+            var convertedCurrency = (amountToConvert * data[toCurrency]).toFixed(4);
             
-            var el = document.getElementById("converted-currency");
-            el.innerHTML = "";
             el.innerHTML = `<p>${convertedCurrency}</p>`;
         });
         
