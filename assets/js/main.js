da8f2e76df83a602b3aaa4e244298a6ebee0714a
Josep <pujol.rabat@gmail.com>
2019-05-15 23:57:57 +0000

    Refractored. Moved functions to file utils.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,16 @@
+// Imports
+import { formatDate, getData } from '/assets/js/utils.js';
+
+
+// Define const and variables
 const baseURL = "https://api.exchangeratesapi.io/"
 const param_latest = "latest?base=EUR&symbols=USD,GBP,JPY,CAD,AUD"
-
-
-function formatDate(dateToFormat) {
-    var dd = dateToFormat.getDate();
-    var mm = dateToFormat.getMonth()+1; 
-    var yyyy = dateToFormat.getFullYear();
-    if(dd<10) { dd='0'+dd;} 
-    if(mm<10) { mm='0'+mm;}
-    return `${yyyy}-${mm}-${dd}`;
-}
 
 var today = new Date();
 var yesterday = new Date(today - 24*60*60*1000);
 var queryDate = formatDate(yesterday);
 var param_hist = `${queryDate}?base=EUR&symbols=USD,GBP,JPY,CAD,AUD`;
 
-
-function getData(url_param, cb) {
-    var xhr = new XMLHttpRequest();
-    var url = baseURL + url_param;
-    console.log(url);
-    xhr.open("GET", url);
-    xhr.send();
-
-    xhr.onreadystatechange = function() {
-        console.log(this.readyState);
-        if (this.readyState == 4 && this.status == 200) {
-            cb(JSON.parse(this.responseText));
-        }
-    };
-}
 
 
 function writeToDocument() {
@@ -73,7 +53,7 @@ function writeToDocument() {
         
             console.log('doc_data', doc_data);
         
-            var el = document.getElementById("testAPI");
+            var el = document.getElementById("latest_rates");
             el.innerHTML = ""
             el.innerHTML = doc_data;
 
---
+++ assets/js/utils.js
@@ -0,0 +1,25 @@
+
+export function formatDate(dateToFormat) {
+    var dd = dateToFormat.getDate();
+    var mm = dateToFormat.getMonth()+1; 
+    var yyyy = dateToFormat.getFullYear();
+    if(dd<10) { dd='0'+dd;} 
+    if(mm<10) { mm='0'+mm;}
+    return `${yyyy}-${mm}-${dd}`;
+}
+
+
+export function getData(url_param, cb) {
+    var xhr = new XMLHttpRequest();
+    var url = baseURL + url_param;
+    console.log(url);
+    xhr.open("GET", url);
+    xhr.send();
+
+    xhr.onreadystatechange = function() {
+        console.log(this.readyState);
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+}
